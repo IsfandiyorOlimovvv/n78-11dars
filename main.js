af546b38040d6574ae308dbb34b6b715229ab42c
IsfandiyorOlimovvv <shoxagaming>
2024-11-02 00:39:41 +0500

    100ball

---
+++ main.js
@@ -0,0 +1,28 @@
+// function sumArray(arr) {
+//   if (arr.length === 0) {
+//       return 0;
+//   }
+//   return arr[0] + sumArray(arr.slice(1));
+// }
+
+
+
+
+// function power(x, n) {
+//   if (n === 0) {
+//       return 1;
+//   }
+//   return x * power(x, n - 1);
+// }
+
+
+
+
+// function reverseString(str) {
+//   if (str === "") {
+//       return "";
+//   }
+//   return reverseString(str.slice(1)) + str[0];
+// }
+
+// console.log(reverseString("hello"));
